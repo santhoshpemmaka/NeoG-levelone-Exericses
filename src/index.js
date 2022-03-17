33e21b9801e1f8185682ec2e285a3511938a4b4f
Santhosh_Pemmaka <santhosh.pemmaka@gmail.com>
2022-03-17 10:04:57 +0530

    implementing mock back-end server.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-
+import {makeServer} from "./server";
+import Mockman from "mockman-js";
+// Call make Server
+makeServer();
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		{/* <App /> */}
+		<Mockman />
 	</React.StrictMode>,
 	document.getElementById("root")
 );
